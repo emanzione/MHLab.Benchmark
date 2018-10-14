02393e531e7002da7dc1ebb06195ac0b2fa059a2
Emanuele Manzione <m4nu.91@gmail.com>
2018-10-14 17:25:09 +0200

    Initial commit

---
+++ MHLab.Benchmark.Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MHLab.Benchmark;
+
+namespace MHLab.Benchmark.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parameters = new BenchmarkParameters()
+            {
+                BenchmarkIterations = 100_000_000,
+                PerformWarmup = true,
+                WarmupAction = null,
+                WarmupIterations = 1000
+            };
+            var result = Benchmarker.Start(ActionToTest, parameters);
+
+            Console.WriteLine("Execution time (TimeSpan): " + result.Elapsed);
+            Console.WriteLine("Execution time (ms): " + result.ElapsedMilliseconds);
+            Console.WriteLine("Execution ticks: " + result.ElapsedTicks);
+            Console.WriteLine("Average execution time (ms): " + result.AverageMilliseconds);
+            Console.WriteLine("Average execution ticks: " + result.AverageTicks);
+            Console.WriteLine("Garbage collections (0): " + result.GarbageCollections0Count);
+            Console.WriteLine("Garbage collections (1): " + result.GarbageCollections1Count);
+            Console.WriteLine("Garbage collections (2): " + result.GarbageCollections2Count);
+
+            Console.ReadLine();
+        }
+
+        private static void ActionToTest()
+        {
+            var r = 52.8f / 3.5f;
+        }
+    }
+}
---
+++ MHLab.Benchmark/Benchmarker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MHLab.Benchmark
+{
+    public struct BenchmarkParameters
+    {
+        public long BenchmarkIterations;
+
+        public bool PerformWarmup;
+        public long WarmupIterations;
+        public Action WarmupAction;
+    }
+
+    public struct BenchmarkResult
+    {
+        public TimeSpan Elapsed;
+        public long ElapsedMilliseconds;
+        public long ElapsedTicks;
+        public long AverageMilliseconds;
+        public long AverageTicks;
+        public int GarbageCollections0Count;
+        public int GarbageCollections1Count;
+        public int GarbageCollections2Count;
+        public long Iterations;
+    }
+
+    public class Benchmarker
+    {
+        public static BenchmarkResult Start(Action actionToProfile, BenchmarkParameters parameters)
+        {
+            // Do a warm-up spin if warmup is enabled
+            if (parameters.PerformWarmup)
+            {
+                parameters.WarmupAction?.Invoke();
+                for (long i = 0; i < parameters.WarmupIterations; i++)
+                {
+                    actionToProfile.Invoke();
+                }
+            }
+
+            // Collect current collections counts, in order to calculate 
+            // them correctly later.
+            var initialCollection0Count = GC.CollectionCount(0);
+            var initialCollection1Count = GC.CollectionCount(1);
+            var initialCollection2Count = GC.CollectionCount(2);
+
+            var iterations = parameters.BenchmarkIterations;
+
+            var stopwatch = Stopwatch.StartNew();
+            for (long i = 0; i < iterations; i++)
+            {
+                // TODO: For generic purpose, I had to call Invoke on the
+                // TODO: passed action. This generates a virtual call,
+                // TODO: that is slower. Find a way to get rid of it.
+                actionToProfile.Invoke();
+            }
+            stopwatch.Stop();
+
+            // Calculate collections counts correctly.
+            var collection0Count = GC.CollectionCount(0) - initialCollection0Count;
+            var collection1Count = GC.CollectionCount(1) - initialCollection1Count;
+            var collection2Count = GC.CollectionCount(2) - initialCollection2Count;
+
+            // Generate the benchmark results.
+            return new BenchmarkResult()
+            {
+                GarbageCollections0Count = collection0Count,
+                GarbageCollections1Count = collection1Count,
+                GarbageCollections2Count = collection2Count,
+                Elapsed = stopwatch.Elapsed,
+                ElapsedMilliseconds = stopwatch.ElapsedMilliseconds,
+                ElapsedTicks = stopwatch.ElapsedTicks,
+                AverageMilliseconds = stopwatch.ElapsedMilliseconds / iterations,
+                AverageTicks = stopwatch.ElapsedTicks / iterations,
+                Iterations = iterations
+            };
+        }
+    }
+}
