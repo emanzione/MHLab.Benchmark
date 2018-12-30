0861f8a7a2387663cb5e87d743526a33a069328e
Emanuele Manzione <m4nu.91@gmail.com>
2018-12-30 16:17:50 +0100

    Added Compare benchmarking feature

--- MHLab.Benchmark.Test/Program.cs
+++ MHLab.Benchmark.Test/Program.cs
@@ -14,6 +14,16 @@ namespace MHLab.Benchmark.Test
                 WarmupAction = null,
                 WarmupIterations = 1000
             };
+            
+            StartSingle(parameters);
+            StartMultiple(parameters);
+            Compare(parameters);
+
+            Console.ReadLine();
+        }
+
+        private static void StartSingle(BenchmarkParameters parameters)
+        {
             var result = Benchmarker.Start(ActionToTest, parameters);
 
             Console.WriteLine("Execution time (TimeSpan): " + result.Elapsed);
@@ -24,13 +34,71 @@ namespace MHLab.Benchmark.Test
             Console.WriteLine("Garbage collections (0): " + result.GarbageCollections0Count);
             Console.WriteLine("Garbage collections (1): " + result.GarbageCollections1Count);
             Console.WriteLine("Garbage collections (2): " + result.GarbageCollections2Count);
+        }
 
-            Console.ReadLine();
+        private static void StartMultiple(BenchmarkParameters parameters)
+        {
+            var actions = new Action[]
+            {
+                ActionToTest,
+                ActionToTest1
+            };
+
+            var results = Benchmarker.Start(actions, parameters);
+
+            for (int i = 0; i < actions.Length; i++)
+            {
+                var result = results[i];
+                Console.WriteLine("Execution time (TimeSpan): " + result.Elapsed);
+                Console.WriteLine("Execution time (ms): " + result.ElapsedMilliseconds);
+                Console.WriteLine("Execution ticks: " + result.ElapsedTicks);
+                Console.WriteLine("Average execution time (ms): " + result.AverageMilliseconds);
+                Console.WriteLine("Average execution ticks: " + result.AverageTicks);
+                Console.WriteLine("Garbage collections (0): " + result.GarbageCollections0Count);
+                Console.WriteLine("Garbage collections (1): " + result.GarbageCollections1Count);
+                Console.WriteLine("Garbage collections (2): " + result.GarbageCollections2Count);
+                Console.WriteLine();
+            }
+        }
+
+        private static void Compare(BenchmarkParameters parameters)
+        {
+            var actions = new Action[]
+            {
+                ActionToTest1
+            };
+
+            var comparisons = Benchmarker.Compare(ActionToTest, parameters, actions);
+
+            Console.WriteLine("Method comparisons executed against ActionToTest method:\n");
+            Console.WriteLine("Method\tTotal ms\tTotal ticks\tAvg. ms\tAvg. ticks\tGC0\tGC1\tGC2\t");
+            for (int i = 0; i < comparisons.Length; i++)
+            {
+                var comparison = comparisons[i];
+
+                var line = actions[i].Method.Name + "\t";
+                line += comparison.ElapsedMillisecondsDifferencePercentage + "% (" +comparison.ElapsedMillisecondsDifference + " ms)\t";
+                line += comparison.ElapsedTicksDifferencePercentage + "% (" + comparison.ElapsedTicksDifference + " ticks)\t";
+                line += comparison.AverageMillisecondsDifferencePercentage + "% (" + comparison.AverageMillisecondsDifference + " ms)\t";
+                line += comparison.AverageTicksDifferencePercentage + "% (" + comparison.AverageTicksDifference + " ticks)\t";
+                line += comparison.GCCollections0DifferencePercentage + "% (" + comparison.GCCollections0Difference + ")\t";
+                line += comparison.GCCollections1DifferencePercentage + "% (" + comparison.GCCollections1Difference + ")\t";
+                line += comparison.GCCollections2DifferencePercentage + "% (" + comparison.GCCollections2Difference + ")\t";
+
+                Console.WriteLine(line);
+            }
         }
 
         private static void ActionToTest()
         {
-            var r = 52.8f / 3.5f;
+            var str = "ABCDEFG";
+            str.Contains("BCD");
+        }
+
+        private static void ActionToTest1()
+        {
+            var str = "ABCDEFG";
+            str.IndexOf("BCD");
         }
     }
 }
--- MHLab.Benchmark/Benchmarker.cs
+++ MHLab.Benchmark/Benchmarker.cs
@@ -25,6 +25,60 @@ namespace MHLab.Benchmark
         public long Iterations;
     }
 
+    public struct BenchmarkComparison
+    {
+        public BenchmarkResult CompareFrom;
+        public BenchmarkResult CompareTo;
+
+        public float ElapsedMillisecondsDifference;
+        public float ElapsedMillisecondsDifferencePercentage;
+
+        public float ElapsedTicksDifference;
+        public float ElapsedTicksDifferencePercentage;
+
+        public float AverageMillisecondsDifference;
+        public float AverageMillisecondsDifferencePercentage;
+
+        public float AverageTicksDifference;
+        public float AverageTicksDifferencePercentage;
+
+        public float GCCollections0Difference;
+        public float GCCollections0DifferencePercentage;
+
+        public float GCCollections1Difference;
+        public float GCCollections1DifferencePercentage;
+
+        public float GCCollections2Difference;
+        public float GCCollections2DifferencePercentage;
+
+        internal void Compare(BenchmarkResult from, BenchmarkResult to)
+        {
+            CompareFrom = from;
+            CompareTo = to;
+
+            ElapsedMillisecondsDifference = to.ElapsedMilliseconds - from.ElapsedMilliseconds;
+            ElapsedMillisecondsDifferencePercentage = (ElapsedMillisecondsDifference / from.ElapsedMilliseconds) * 100;
+
+            ElapsedTicksDifference = to.ElapsedTicks - from.ElapsedTicks;
+            ElapsedTicksDifferencePercentage = (ElapsedTicksDifference / from.ElapsedTicks) * 100;
+
+            AverageMillisecondsDifference = to.AverageMilliseconds - from.AverageMilliseconds;
+            AverageMillisecondsDifferencePercentage = (AverageMillisecondsDifference / from.AverageMilliseconds) * 100;
+
+            AverageTicksDifference = to.AverageTicks - from.AverageTicks;
+            AverageTicksDifferencePercentage = (AverageTicksDifference / from.AverageTicks) * 100;
+
+            GCCollections0Difference = to.GarbageCollections0Count - from.GarbageCollections0Count;
+            GCCollections0DifferencePercentage = (GCCollections0Difference / from.GarbageCollections0Count) * 100;
+
+            GCCollections1Difference = to.GarbageCollections1Count - from.GarbageCollections1Count;
+            GCCollections1DifferencePercentage = (GCCollections1Difference / from.GarbageCollections1Count) * 100;
+
+            GCCollections2Difference = to.GarbageCollections2Count - from.GarbageCollections2Count;
+            GCCollections2DifferencePercentage = (GCCollections2Difference / from.GarbageCollections2Count) * 100;
+        }
+    }
+
     public class Benchmarker
     {
         public static BenchmarkResult Start(Action actionToProfile, BenchmarkParameters parameters)
@@ -87,6 +141,22 @@ namespace MHLab.Benchmark
             }
 
             return results;
+        }
+
+        public static BenchmarkComparison[] Compare(Action mainAction, BenchmarkParameters parameters, params Action[] actions)
+        {
+            var mainResult = Start(mainAction, parameters);
+            var otherResults = Start(actions, parameters);
+
+            var comparisons = new BenchmarkComparison[actions.Length];
+            for (int i = 0; i < actions.Length; i++)
+            {
+                var comparison = new BenchmarkComparison();
+                comparison.Compare(mainResult, otherResults[i]);
+                comparisons[i] = comparison;
+            }
+
+            return comparisons;
         }
     }
 }
